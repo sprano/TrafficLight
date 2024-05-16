d43c6ec8abcbfd742857c6fb65f9ff07779c8e32
Sofia <sofiapanoch@gmail.com>
2024-05-16 12:12:01 +0300

    Turn a traffic light on

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Color {
+    case red
+    case yellow
+    case green
+}
 
+final class ViewController: UIViewController {
+
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 64
+        yellowLight.layer.cornerRadius = 64
+        greenLight.layer.cornerRadius = 64
+        startButton.layer.cornerRadius = 10
+        redLight.layer.opacity = 0.3
+        yellowLight.layer.opacity = 0.3
+        greenLight.layer.opacity = 0.3
     }
-
-
+    
+    var color = Color.red
+    
+    @IBAction func turnTrafficLightOn() {
+        startButton.setTitle(
+            startButton.isSelected ? "START" : "NEXT", for: .normal
+            )
+        
+        switch color {
+        case .red:
+            redLight.layer.opacity = 1
+            yellowLight.layer.opacity = 0.3
+            greenLight.layer.opacity = 0.3
+            color = .yellow
+        case .yellow:
+            redLight.layer.opacity = 0.3
+            yellowLight.layer.opacity = 1
+            greenLight.layer.opacity = 0.3
+            color = .green
+        case .green:
+            redLight.layer.opacity = 0.3
+            yellowLight.layer.opacity = 0.3
+            greenLight.layer.opacity = 1
+            color = .red
+        }
+    }
 }
 
